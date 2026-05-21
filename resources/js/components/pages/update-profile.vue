4cdf9b05b0072777241682d6e95e410b77ce9a7d
Liam <liamag2000@protonmail.com>
2026-05-22 00:21:50 +0100

    feature/88
    Update Web.php for better readability.
    
    Update NavigationBar for new route mentions and change logout to a post request for better security.
    
    Update Make post and update profile page for new route names.

--- resources/js/components/pages/update-profile.vue
+++ resources/js/components/pages/update-profile.vue
@@ -9,7 +9,7 @@
         </div>
       <hr />
         <div>
-            <Form :action="route('user.edit', $page.props.auth.user.id)" method="put"
+            <Form :action="route('user.update', $page.props.auth.user.id)" method="put"
                   #default = '{
                     processing,
                     isDirty,
@@ -36,7 +36,7 @@
                     </div>
                 </div>
             </Form>
-            <Form :action="route('user.edit', $page.props.auth.user.id)" method="put"
+            <Form :action="route('user.update', $page.props.auth.user.id)" method="put"
                   #default = '{
                     processing,
                     isDirty,
@@ -63,7 +63,7 @@
                     </div>
                 </div>
             </Form>
-            <Form :action="route('user.edit', $page.props.auth.user.id)" method="put"
+            <Form :action="route('user.update', $page.props.auth.user.id)" method="put"
                   #default = '{
                     processing,
                     isDirty,
@@ -91,7 +91,7 @@
                     </div>
                 </div>
             </Form>
-            <Form :action="route('user.edit', $page.props.auth.user.id)" method="put"
+            <Form :action="route('user.update', $page.props.auth.user.id)" method="put"
                   #default = '{
                     processing,
                     isDirty,
@@ -118,7 +118,7 @@
                     </div>
                 </div>
             </Form>
-            <Form :action="route('user.edit', $page.props.auth.user.id)" method="put"
+            <Form :action="route('user.update', $page.props.auth.user.id)" method="put"
                   #default = '{
                     processing,
                     isDirty,
@@ -145,7 +145,7 @@
                 </div>
             </div>
             </Form>
-            <Form :action="route('user.edit', $page.props.auth.user.id)" method="put"
+            <Form :action="route('user.update', $page.props.auth.user.id)" method="put"
                   #default = '{
                     processing,
                     isDirty,
